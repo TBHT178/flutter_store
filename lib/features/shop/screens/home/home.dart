fb8876e2766ff5b27b3624a2dc44571752c2828f
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-06-27 18:35:12 +0700

    Create Search Bar and Horizontal ListView builder Categories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_store/common/widgets/appbar/appbar.dart';
 import 'package:flutter_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:flutter_store/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:flutter_store/utils/constants/colors.dart';
+import 'package:flutter_store/utils/constants/image_strings.dart';
 import 'package:flutter_store/utils/constants/sizes.dart';
 import 'package:flutter_store/utils/device/device_utility.dart';
 import 'package:flutter_store/utils/helpers/helper_functions.dart';
@@ -10,7 +14,9 @@ import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -32,8 +38,33 @@ class HomeScreen extends StatelessWidget {
                   ),
 
                   /// -- Searchbar
-                  TSearchContainer(text: 'Search in Store',),
-                  /// -- Categories
+                  TSearchContainer(
+                    text: 'Search in Store',
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  /// -- Categories Bar
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        /// -- Heading
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                          textColor: Colors.white,
+                        ),
+                        SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+
+                        /// -- Categories
+                        THomeCategories()
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -43,5 +74,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
