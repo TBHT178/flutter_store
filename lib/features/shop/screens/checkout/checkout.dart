22997da7348ded15df494de4b2c327740d68c4a3
Tran Bao Huyen Tram <tranbaohuyentram96@gmail.com>
2024-07-01 17:16:50 +0700

    Move file to correct folder

---
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_store/common/widgets/appbar/appbar.dart';
+import 'package:flutter_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:flutter_store/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:flutter_store/utils/constants/colors.dart';
+import 'package:flutter_store/utils/constants/sizes.dart';
+import 'package:flutter_store/utils/helpers/helper_functions.dart';
+
+class CheckoutScreen extends StatelessWidget {
+  const CheckoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text(
+          'Order Review',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Items in Cart
+              const TCartItems(
+                showAddRemoveButtons: false,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              /// Coupon TextField
+              TRoundedContainer(
+                showBorder: true,
+                backgroundColor: dark ? TColors.dark : TColors.white,
+                padding: const EdgeInsets.only(
+                    top: TSizes.sm,
+                    bottom: TSizes.sm,
+                    right: TSizes.sm,
+                    left: TSizes.md),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Have a promo code? Enter here',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
